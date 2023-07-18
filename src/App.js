1131dca8a5cf32b247924b96abee2f35abb8e492
Chelly Compendio <chelly.compendio@nytimes.com>
2023-07-18 14:13:12 -0400

    home screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import YourNYT from './pages/YourNYT';
-import './App.css';
+import './App.scss';
 import {StrictMode } from "react"; 
 import logo from "./LogoWhite.png"
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 
-import '../App.css';
+import '../App.scss';
 import React from 'react';
 import {Outlet, Link} from "react-router-dom";
 
 function Home() {
     return (
         <>
-        <nav>
-            <h1>Home</h1>
+        <div className="home">
+            <p className="home__text">
+                Welcome, Grace R. to your
+            </p>
+            <p className="home__year">
+                2023
+            </p>
+            <p className="home__text">
+                year in review
+            </p>
+            <p className="home__text">
+                Your profile leans Purple
+            </p>
             <Link to= "/yournyt"> Begin </Link>
-        </nav>
+        </div>
         <Outlet />
         </>
     );
