8fb35f16774f57c96928509926e904f56615e969
Neha Shah <nehashah@A15346.local>
2023-07-19 14:57:01 -0400

    Added Home page details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,20 +7,28 @@ function Home() {
     return (
         <>
             <div className="home">
-                <p className="home__text">
-                    Welcome, Grace R. to your
+                <p className="home__name">
+                    Welcome
+                </p>
+                <p className="home__name">
+                Grace R. 
+                </p>
+                <p className="home__name">
+                to your
                 </p>
                 <p className="home__year">
                     2023
                 </p>
-                <p className="home__text">
+                <p className="home__name">
                     year in review
                 </p>
-                <p className="home__text">
+                <br></br>
+                <p className="home__name">
                     Your profile leans Purple
                 </p>
                 <Link className="button" to="/yournyt"> Begin </Link>
-            </div>
+                </div>
+               
             <Outlet />
         </>
     );
