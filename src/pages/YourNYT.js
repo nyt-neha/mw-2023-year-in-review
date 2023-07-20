e9eb632055cce50efb1a805bc9093c9f8fb55b8c
Chelly Compendio <chelly.compendio@nytimes.com>
2023-07-20 13:16:24 -0400

    code for number of artricles words and contributors

--- src/pages/YourNYT.js
+++ src/pages/YourNYT.js
@@ -1,22 +1,37 @@
 
 import '../App.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Outlet, Link} from "react-router-dom";
 
+function numWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function YourNYT(props) {
+    const [articleNum, setArticleNum] = useState(0);
+    const [wordNum, setWordNum] = useState(0);
+    const [contNum, setContNum] = useState(0);
+
+
+    useEffect(() => {
+      setArticleNum(1235)
+      setWordNum(1587939)
+      setContNum(11)
+    });
+  
     return (
         <>
             <div className="yournyt">
                 <div className="stat">
-                    <p className="stat__num">1,235</p>
+                    <p className="stat__num">{numWithCommas(articleNum)}</p>
                     <p className="stat__name">Articles {String.fromCharCode(8594)}</p>
                 </div>
                 <div className="stat">
-                    <p className="stat__num">1,587,939</p>
+                <p className="stat__num">{numWithCommas(wordNum)}</p>
                     <p className="stat__name">Words {String.fromCharCode(8594)}</p>
                 </div>
                 <div className="stat">
-                    <p className="stat__num">11</p>
+                <p className="stat__num">{numWithCommas(contNum)}</p>
                     <p className="stat__name">Favorite {<br/>} Contributors {String.fromCharCode(8594)}</p>
                 </div>
                 <div className="button">Share</div>
